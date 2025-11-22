703e38b12e4aef5fbd853b73f5c9100d0e4c6f12
김진수 <rlawlstn3206@gmail.com>
2025-11-23 03:04:18 +0900

    feat: github oauth 추가

--- src/main/java/com/example/commitwhisper/security/CustomOAuth2UserService.java
+++ src/main/java/com/example/commitwhisper/security/CustomOAuth2UserService.java
@@ -3,6 +3,7 @@ package com.example.commitwhisper.security;
 import com.example.commitwhisper.entity.User;
 import com.example.commitwhisper.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -10,6 +11,10 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * Github 용
+ */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
@@ -19,50 +24,60 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     @Override
     @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2User oAuth2User = super.loadUser(userRequest);
-
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
-        String providerId = getProviderId(oAuth2User, registrationId);
-        String name = getName(oAuth2User, registrationId);
-        String email = getEmail(oAuth2User, registrationId);
-        String loginId = email != null ? email : (registrationId + "_" + providerId);
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        
+        // GitHub 전용 파싱
+        String providerId = extractProviderId(oAuth2User);
+        String name = extractName(oAuth2User);
+        String email = extractEmail(oAuth2User);
+        String loginId = extractLoginId(oAuth2User, email, providerId);
 
-        User user = userRepository.findByProviderAndProviderId(registrationId, providerId)
+        User user = findOrCreateUser(registrationId, providerId, loginId, name, email);
+        UserPrincipalDto userDto = createUserPrincipalDto(user);
+        
+        return new UserPrincipal(userDto, oAuth2User.getAttributes());
+    }
+
+    private String extractProviderId(OAuth2User oAuth2User) {
+        Object id = oAuth2User.getAttribute("id");
+        return id != null ? id.toString() : oAuth2User.getName();
+    }
+
+    private String extractName(OAuth2User oAuth2User) {
+        String name = oAuth2User.getAttribute("name");
+        // GitHub의 경우 name이 없으면 login 사용
+        return name != null ? name : oAuth2User.getAttribute("login");
+    }
+
+    private String extractEmail(OAuth2User oAuth2User) {
+        return oAuth2User.getAttribute("email");
+    }
+
+    private String extractLoginId(OAuth2User oAuth2User, String email, String providerId) {
+        // GitHub의 경우 login을 우선 사용
+        String login = oAuth2User.getAttribute("login");
+        if (login != null) {
+            return login;
+        }
+        return email != null ? email : ("github_" + providerId);
+    }
+
+    private User findOrCreateUser(String registrationId, String providerId, String loginId, String name, String email) {
+        return userRepository.findByProviderAndProviderId(registrationId, providerId)
             .orElseGet(() -> {
                 User newUser = new User(loginId, name, registrationId, providerId, email);
                 return userRepository.save(newUser);
             });
+    }
 
-        UserPrincipalDto userDto = new UserPrincipalDto(
+    private UserPrincipalDto createUserPrincipalDto(User user) {
+        return new UserPrincipalDto(
             user.getId(),
             user.getLoginId(),
             user.getPassword(),
             user.getName()
         );
-
-        return new UserPrincipal(userDto, oAuth2User.getAttributes());
-    }
-
-    private String getProviderId(OAuth2User oAuth2User, String registrationId) {
-        if ("google".equals(registrationId)) {
-            Object sub = oAuth2User.getAttribute("sub");
-            return sub != null ? sub.toString() : oAuth2User.getName();
-        }
-        return oAuth2User.getName();
-    }
-
-    private String getName(OAuth2User oAuth2User, String registrationId) {
-        if ("google".equals(registrationId)) {
-            return oAuth2User.getAttribute("name");
-        }
-        return oAuth2User.getAttribute("name");
-    }
-
-    private String getEmail(OAuth2User oAuth2User, String registrationId) {
-        if ("google".equals(registrationId)) {
-            return oAuth2User.getAttribute("email");
-        }
-        return oAuth2User.getAttribute("email");
     }
 }
 
--- src/main/java/com/example/commitwhisper/security/CustomOidcUserService.java
+++ src/main/java/com/example/commitwhisper/security/CustomOidcUserService.java
@@ -3,6 +3,7 @@ package com.example.commitwhisper.security;
 import com.example.commitwhisper.entity.User;
 import com.example.commitwhisper.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 /**
  * Google Kakao 용
  */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CustomOidcUserService extends OidcUserService {
@@ -22,28 +24,40 @@ public class CustomOidcUserService extends OidcUserService {
     @Override
     @Transactional
     public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
-        OidcUser oidcUser = super.loadUser(userRequest);
-
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        OidcUser oidcUser = super.loadUser(userRequest);
+        
+        // OIDC 공통 파싱 (Google, Kakao 등)
         String providerId = oidcUser.getSubject();
         String name = oidcUser.getFullName();
         String email = oidcUser.getEmail();
-        String loginId = email != null ? email : (registrationId + "_" + providerId);
+        String loginId = extractLoginId(email, registrationId, providerId);
 
-        User user = userRepository.findByProviderAndProviderId(registrationId, providerId)
+        User user = findOrCreateUser(registrationId, providerId, loginId, name, email);
+        UserPrincipalDto userDto = createUserPrincipalDto(user);
+        
+        return new UserPrincipal(userDto, oidcUser.getClaims(), oidcUser.getIdToken(), oidcUser.getUserInfo());
+    }
+
+    private String extractLoginId(String email, String registrationId, String providerId) {
+        return email != null ? email : (registrationId + "_" + providerId);
+    }
+
+    private User findOrCreateUser(String registrationId, String providerId, String loginId, String name, String email) {
+        return userRepository.findByProviderAndProviderId(registrationId, providerId)
             .orElseGet(() -> {
                 User newUser = new User(loginId, name, registrationId, providerId, email);
                 return userRepository.save(newUser);
             });
+    }
 
-        UserPrincipalDto userDto = new UserPrincipalDto(
+    private UserPrincipalDto createUserPrincipalDto(User user) {
+        return new UserPrincipalDto(
             user.getId(),
             user.getLoginId(),
             user.getPassword(),
             user.getName()
         );
-
-        return new UserPrincipal(userDto, oidcUser.getClaims(), oidcUser.getIdToken(), oidcUser.getUserInfo());
     }
 }
 
